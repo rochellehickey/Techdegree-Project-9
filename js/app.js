4509d928c33bff86942b509874aca9c2c4c9f6cc
Rochelle Hickey <rochellehickey@gmail.com>
2019-03-07 15:15:17 -0500

    Working on chart.js doughnut chart

--- js/app.js
+++ js/app.js
@@ -1,2 +1,9 @@
+// Alert box
+const closeButton = document.querySelector(".close-button");
+const alertBox = document.querySelector(".alert-box");
+const hidden = document.querySelector(".hidden");
 
-
+closeButton.addEventListener('click', function() {
+  // adds/toggles class of hidden to alert-box which removes it from the DOM
+  alertBox.classList.toggle('hidden');
+});
